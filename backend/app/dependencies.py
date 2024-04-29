2a044a99a13ca36572dd4fdb63955ce7c9d173d9
erikrozi <erikkrozi@gmail.com>
2024-04-28 21:23:50 -0700

    Adding basic langchain summarization functionality in API
    Co-authored-by: Poojan Pandya <poojan-pandya@users.noreply.github.com>

---
+++ backend/app/dependencies.py
@@ -0,0 +1,24 @@
+from fastapi import Depends, FastAPI
+from fastapi.security import OAuth2PasswordBearer
+from pydantic import BaseModel
+
+from typing import Union
+from typing_extensions import Annotated
+
+
+class User(BaseModel):
+    username: str
+    email: Union[str, None] = None
+    full_name: Union[str, None] = None
+    disabled: Union[bool, None] = None
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+def fake_decode_token(token):
+    return User(
+        username=token + "fakedecoded", email="john@example.com", full_name="John Doe"
+    )
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    user = fake_decode_token(token)
+    return user
