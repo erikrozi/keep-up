d603f58494e9e2a9daa42bda74deaf86ba0a0545
PritiRangnekar <priti.rangnekar@gmail.com>
2024-05-23 03:50:01 -0700

    getting topics and subtopics from supabase

--- frontend/src/pages/OnboardingFive.js
+++ frontend/src/pages/OnboardingFive.js
@@ -1,11 +1,14 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import { Checkbox } from "../components/ui/checkbox.tsx";
 import { useForm } from "react-hook-form";
 import ProgressBar from '../components/ui/progressbar.js';
 import { Button } from "../components/ui/button.tsx";
-import useSupabaseUser from '../hooks/useSupabaseUser'
+import useSupabaseUser from '../hooks/useSupabaseUser';
+
+import { createClient } from "@supabase/supabase-js";
+
 
 import {
     Form,
@@ -15,38 +18,48 @@ import {
     FormLabel,
 } from "../components/ui/form.tsx"
 
-const subtopicMap = {
-    "Machine Learning": ['Gradient Descent', 'Momentum', 'Confusion Matrix', 'Regularization'],
-    "Cybersecurity": ['Trust and Safety', 'Threats', 'Offensive', 'Defensive'],
-    "Human-Computer Interaction": ['VR', 'AR', 'UX', 'Ethics'],
-    "Policy": ['AI', 'Security', 'Blockchain', 'Compute'],
-    "Systems": ['Databases', 'Operating Systems', 'Networking', 'Compilers'],
-    "Computational Biology": ['Genomics', 'Bioinformatics', 'Differential Equations', 'Numerical Analysis'],
-    "Natural Language Processing": ['Linguistics', 'ChatGPT', 'Claude', 'Llama'],
-    "Theory": ['Cryptography', 'Linear Algebra', 'Discrete Math', 'Optimization'],
-    "Data Science": ["Statistics", "Data Engineering", "Python", "R"],
-    "Computer Vision": ["Multimodal", "CNNs", "Object Detection", "Segmentation"]
-};
+const supabase = createClient("https://jmrqzntxxgtbbuurhivj.supabase.co", 
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImptcnF6bnR4eGd0YmJ1dXJoaXZqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTU2MzI3NTcsImV4cCI6MjAzMTIwODc1N30.bnssrh3cwo41hAUb90XmVQT8iZNgWLEl-lzC7GUaQJQ");
 
 function OnboardingFive() {
     const navigate = useNavigate();
     const form = useForm();
 
     const { user, loading, error } = useSupabaseUser();
+
+    const [topics, setTopics] = useState([]);
+    const [subtopics, setSubtopics] = useState([]);
+
+
     const [selectedTopics, setSelectedTopics] = useState([]);
     const [selectedSubtopics, setSelectedSubtopics] = useState([]);
     const [errorMessage, setErrorMessage] = useState('');
 
-    const topics = [
-        'Machine Learning', 'Cybersecurity', 'Human-Computer Interaction', 'Policy', 'Systems',
-        'Computational Biology', 'Natural Language Processing', 'Theory', 'Data Science', 'Computer Vision'
-    ];
+
+    useEffect(() => {
+      getTopics();
+      getSubtopics();
+    }, []);
+
+
+    async function getTopics() {
+      const { data } = await supabase.from("topics").select("topic_id, topic_name");
+      setTopics(data);
+    }
+
+    async function getSubtopics() {
+      const { data } = await supabase.from("subtopics").select("subtopic_id, topic_id, subtopic_name");
+      setSubtopics(data);
+    }
 
     const handleCheckboxChange = (topic) => {
         const isSelected = selectedTopics.includes(topic);
         if (isSelected) {
-            setSelectedTopics(selectedTopics.filter((item) => item !== topic));
-            setSelectedSubtopics(selectedSubtopics.filter((subtopic) => !subtopicMap[topic].includes(subtopic)));
+            setSelectedTopics(selectedTopics.filter((item) => item.topic_id !== topic.topic_id));
+
+            const associatedSubtopicIds = subtopics.filter(subtopic => subtopic.topic_id === topic.topic_id).map(subtopic => subtopic.subtopic_id);
+            setSelectedSubtopics(selectedSubtopics.filter((subtopic) => !associatedSubtopicIds.includes(subtopic.subtopic_id)));
+
             setErrorMessage(''); // Clear error message when unchecking
         } else {
             if (selectedTopics.length < 5) {
@@ -61,7 +74,8 @@ function OnboardingFive() {
     const handleSubtopicChange = (subtopic) => {
         const isSelected = selectedSubtopics.includes(subtopic);
         if (isSelected) {
-            setSelectedSubtopics(selectedSubtopics.filter((item) => item !== subtopic));
+            setSelectedSubtopics(selectedSubtopics.filter((item) => item.subtopic_id !== subtopic.subtopic_id));
+            setErrorMessage(''); // Clear error message when unchecking
         } else {
             if (selectedSubtopics.length < 15) {
                 setSelectedSubtopics([...selectedSubtopics, subtopic]);
@@ -79,7 +93,7 @@ function OnboardingFive() {
 
     return (
         <div className="bg-gradient-to-r from-skyblue-500 via-white-500 to-royal-blue-500 flex justify-center items-center min-h-screen">
-            <div className="bg-card border border-gray-200 shadow-lg rounded-lg p-6">
+            <div className="bg-card border border-gray-400 shadow-lg rounded-lg p-6">
                 <h1 className="mb-4 text-4xl font-bold leading-none tracking-tight text-gray-900">Welcome!</h1>
                 <h2 className="mb-4 text-2xl font-normal text-gray-500">We'd like to get to know you a bit more.</h2>
                 <Form {...form}>
@@ -103,7 +117,7 @@ function OnboardingFive() {
                                             </FormControl>
                                             <div className="space-y-1 leading-none">
                                                 <FormLabel>
-                                                    {topic}
+                                                    {topic.topic_name}
                                                 </FormLabel>
                                             </div>
                                         </FormItem>
@@ -111,14 +125,14 @@ function OnboardingFive() {
                                 />
                                 {selectedTopics.includes(topic) && (
                                     <div className="ml-8 mt-2 flex flex-wrap">
-                                        {subtopicMap[topic].map((subtopic) => (
+                                        {subtopics.filter(subtopic => subtopic.topic_id === topic.topic_id).map((subtopic) => (
                                             <button
                                                 type="button"
                                                 key={subtopic}
                                                 className={`m-1 px-2 py-1 rounded-full border ${selectedSubtopics.includes(subtopic) ? 'bg-blue-500 text-white' : 'bg-gray-200 text-gray-800'}`}
                                                 onClick={() => handleSubtopicChange(subtopic)}
                                             >
-                                                {subtopic}
+                                                {subtopic.subtopic_name}
                                             </button>
                                         ))}
                                     </div>
